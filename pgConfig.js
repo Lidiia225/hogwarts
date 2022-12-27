05d67c9d97503cbbc99778315fe40cb0879c3e14
Lidiia225 <lida200q@gmail.com>
2022-12-27 09:32:56 +0000

    second commit

---
+++ pgConfig.js
@@ -0,0 +1,12 @@
+//Импортируем класс Pool из модуля pg
+
+//import dotenv from 'dotenv'
+import pg from "pg";
+//dotenv.config()
+
+
+//Создаём пул соединений с сервером.
+export const pool = new pg.Pool({connectionString: process.env.DB_URL,
+	    ssl: { rejectUnauthorized: false}
+})
+
